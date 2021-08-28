678f57e5e46d1189b50587df168dcc30bf1c07e2
jet2018 <ezrajet9@gmail.com>
2021-08-28 18:34:04 +0300

    finished more tests

--- src/components/brief_article.vue
+++ src/components/brief_article.vue
@@ -1,11 +1,28 @@
 <template>
-  <div class="card p-2" >
+  <div class="card p-2">
     <b-media left-align horizontal-align="center" class="mt-1">
       <template #aside>
-        <b-img v-if="introductory_file" :src="introductory_file" width="150" alt="placeholder"></b-img>
+        <b-img
+          v-if="introductory_file"
+          :src="introductory_file"
+          width="150"
+          alt="placeholder"
+        ></b-img>
       </template>
-      <small class="text-secondary"><Chip v-bind:class="['text-capitalize text-light', 'bg-'+blog_color]" :label="'By '+full_name" :image="image+poster_image" /> <span class="float-right">Added <timeago :datetime="posted_on" :auto-update="60"></timeago></span></small>
-      <router-link to="/" v-bind:class="['mt-0 mb-1 h5 d-block text-primary']">{{title}}</router-link>
+      <small class="text-secondary"
+        ><Chip
+          v-bind:class="['text-capitalize text-light', 'bg-' + blog_color]"
+          :label="'By ' + full_name"
+          :image="image + poster_image" />
+        <span class="float-right"
+          >Added
+          <timeago :datetime="posted_on" :auto-update="60"></timeago></span
+      ></small>
+      <router-link
+        to="/"
+        v-bind:class="['mt-0 mb-1 h5 d-block text-primary']"
+        >{{ title }}</router-link
+      >
       <!-- <truncate
       class="mb-0"
       collapsed-text-class="collapsed "
@@ -16,106 +33,142 @@
       :text="body"
       ></truncate> -->
       <div>
-          <span><router-link v-for="cat in category" :key="cat"  to="#" v-text="'#'+cat" icon="pi pi-trash" /></span>&nbsp;
-          <span><router-link v-for="cat in sub_category" class="text-sm text-info" :key="cat"  to="#" v-text="'##'+cat" icon="pi pi-trash" /></span>
+        <span
+          ><router-link
+            v-for="cat in category"
+            :key="cat"
+            to="#"
+            v-text="'#' + cat"
+            icon="pi pi-trash" /></span
+        >&nbsp;
+        <span
+          ><router-link
+            v-for="cat in sub_category"
+            class="text-sm text-info"
+            :key="cat"
+            to="#"
+            v-text="'##' + cat"
+            icon="pi pi-trash"
+        /></span>
       </div>
+
       <!-- to be hidden to unauthenticated users -->
       <v-row cols="12">
-      <v-col lg="12">
-        <span class="p-buttonset">
-          <Button :label="total_downvotes"  class="p-button-text" icon="pi pi-thumbs-down" />
-          <Button :label="total_upvotes"  class="p-button-text" icon="pi pi-thumbs-up" />
-          <Button label="Bookmark"  class="p-button-text" icon="pi pi-bookmark" />
-          <Button v-if ="total_comments>0" :label="total_comments+' comments'" class="p-button-text" icon="pi pi-comments" />
-          <Button v-else label="Add a comment" class="p-button-text" icon="pi pi-comments" />
-
-        </span>
-      </v-col>
-    </v-row>
+        <v-col lg="12">
+          <span class="p-buttonset">
+            <Button
+              :label="total_downvotes"
+              class="p-button-text"
+              icon="pi pi-thumbs-down"
+            />
+            <Button
+              :label="total_upvotes"
+              class="p-button-text"
+              icon="pi pi-thumbs-up"
+            />
+            <Button
+              label="Bookmark"
+              class="p-button-text"
+              icon="pi pi-bookmark"
+            />
+            <Button
+              v-if="total_comments > 0"
+              :label="total_comments + ' comments'"
+              class="p-button-text"
+              icon="pi pi-comments"
+            />
+            <Button
+              v-else
+              label="Add a comment"
+              class="p-button-text"
+              icon="pi pi-comments"
+            />
+          </span>
+        </v-col>
+      </v-row>
     </b-media>
-
   </div>
 </template>
 <script>
-  // import truncate from 'vue-truncate-collapsed';
-  import Chip from "primevue/chip"
-  import Button from "primevue/button"
-  import config from "@/config"
+// import truncate from 'vue-truncate-collapsed';
+import Chip from "primevue/chip";
+import Button from "primevue/button";
+import config from "@/config";
 
-  export default {
-    components:{
-      // truncate,
-      Button,
-      Chip,
-    },
-    data () {
-      return {
-        image:config.images
-      };
-    },
-    name: "briefArticle",
-    props: {
-    blog_color:{
+export default {
+  components: {
+    // truncate,
+    Button,
+    Chip,
+  },
+  data() {
+    return {
+      image: config.images,
+    };
+  },
+  name: "briefArticle",
+  props: {
+    blog_color: {
       type: String, // String, Number, Boolean, Function, Object, Array
       required: true,
-      default: null
+      default: null,
     },
-    total_upvotes:{
+    total_upvotes: {
       type: Number, // String, Number, Boolean, Function, Object, Array
       required: true,
-      default: 0
+      default: 0,
     },
-    total_comments:{
+    total_comments: {
       type: Number, // String, Number, Boolean, Function, Object, Array
       required: true,
-      default: 0
+      default: 0,
     },
 
-    total_downvotes:{
+    total_downvotes: {
       type: Number, // String, Number, Boolean, Function, Object, Array
       required: true,
-      default: 0
+      default: 0,
     },
     title: {
       type: String, // String, Number, Boolean, Function, Object, Array
       required: true,
-      default: null
+      default: null,
     },
-    full_name:{
+    full_name: {
       type: String, // String, Number, Boolean, Function, Object, Array
       required: true,
-      default: null
+      default: null,
     },
     body: {
       type: String, // String, Number, Boolean, Function, Object, Array
       required: true,
-      default: null
+      default: null,
     },
-    introductory_file:{
+    introductory_file: {
       type: String, // String, Number, Boolean, Function, Object, Array
       required: true,
-      default: null
+      default: null,
     },
-    poster_image:{
+    poster_image: {
       type: String, // String, Number, Boolean, Function, Object, Array
       required: true,
-      default: null
+      default: null,
     },
-    category:{
+    category: {
       type: Array, // String, Number, Boolean, Function, Object, Array
       required: true,
-      default: null
+      default: null,
     },
-    sub_category:{
+    sub_category: {
       type: String, // String, Number, Boolean, Function, Object, Array
       required: true,
-      default: null
+      default: null,
     },
-    posted_on:{
+    posted_on: {
       type: Date, // String, Number, Boolean, Function, Object, Array
       required: true,
-      default: null
-    }
-  }
+      default: null,
+    },
+  },
 };
 </script>
